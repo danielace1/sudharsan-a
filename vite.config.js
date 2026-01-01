9d2546dff871d2ee8c09a60718c381a78b1c250c
danielace1 <danielsudharsan1805@gmail.com>
2026-01-01 16:24:04 +0530

    feat: og tags, pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Sudharsan A - Portfolio",
+        short_name: "Sudharsan",
+        description: "Crafting elegant and intuitive digital experiences.",
+        theme_color: "#0ea5e9",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/favicon/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
